95946ed27e2f946cfa33213af850a2dea437651e
Diogo Jose <124062577+Diogjos@users.noreply.github.com>
2023-09-01 18:32:05 -0300

    swith

--- account/AccountTest.java
+++ account/AccountTest.java
@@ -7,21 +7,21 @@ public class AccountTest {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
-		Account myaccount = new Account(" Diogo",100);
+		Account myaccount = new Account("",0);
 		
-		System.out.println(" Account is :" + myaccount.getName() + " | " + 
-		myaccount.getBalance());
-		
-		System.out.println(" Enter your deposit: ");
+	
+		System.out.println("Please enter the name: ");
+		String theName = input.nextLine();
+		myaccount.setName(theName);
+		System.out.println("Enter your deposit: ");
 		double depositAmmount = input.nextDouble();
-		System.out.println("adding to account balance :" + depositAmmount);
+		System.out.println("Adding to account balance :" + depositAmmount);
 		myaccount.deposit(depositAmmount);
 		System.out.println(myaccount.getName() + " your ammount is: " + 
 		myaccount.getBalance() + " R$");
-		
-		System.out.println(" Enter your debit:  ");
+		System.out.println("Enter your debit:  ");
 		double withdrawAmmount = input.nextDouble();
-		System.out.println("adding to account balance: " + withdrawAmmount);
+		System.out.println("Removing to account balance: " + withdrawAmmount);
 		myaccount.withdraw(withdrawAmmount);
 		System.out.println(myaccount.getName() + " your ammount is: " + 
 		myaccount.getBalance() + " R$");
